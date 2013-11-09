7e180ed81f669b24b7d88303be168c7ed66924da
Maciej Chmurski <contact@t2n.eu>
2013-11-09 02:02:20 +0100

    damn sockets

--- server.js
+++ server.js
@@ -41,6 +41,12 @@ if ('development' === app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function() {
+var server = http.createServer(app);
+server.listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
+});
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(socket) {
+	socket.emit('hello');
 });
