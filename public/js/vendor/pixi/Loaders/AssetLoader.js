6856462d8f7525e299e2e70ae5ab7cce24fee9c0
lukasz-wojciak <kontakt@wojciak.com.pl>
2013-11-09 01:39:07 +0100

    +Create initial client side layout

---
+++ public/js/vendor/pixi/Loaders/AssetLoader.js
@@ -0,0 +1,99 @@
+/**
+ * @author Mat Groves http://matgroves.com/ @Doormat23
+ */
+
+/**
+ * A Class that loads a bunch of images / sprite sheet / bitmap font files. Once the assets have been loaded they are added to the PIXI Texture cache and can be accessed easily through PIXI.Texture.fromImage() and PIXI.Sprite.fromImage()
+ * When all items have been loaded this class will dispatch a "onLoaded" event
+ * As each individual item is loaded this class will dispatch a "onProgress" event
+ * @class AssetLoader
+ * @constructor
+ * @extends EventTarget
+ * @param {Array} assetURLs an array of image/sprite sheet urls that you would like loaded supported. Supported image formats include "jpeg", "jpg", "png", "gif". Supported sprite sheet data formats only include "JSON" at this time. Supported bitmap font data formats include "xml" and "fnt".
+ */
+PIXI.AssetLoader = function(assetURLs)
+{
+  PIXI.EventTarget.call(this);
+
+  /**
+   * The array of asset URLs that are going to be loaded
+   * @property assetURLs
+   * @type Array
+   */
+  this.assetURLs = assetURLs;
+
+  this.crossorigin = false;
+
+    this.loadersByType = {
+        "jpg":  PIXI.ImageLoader,
+        "jpeg": PIXI.ImageLoader,
+        "png":  PIXI.ImageLoader,
+        "gif":  PIXI.ImageLoader,
+        "json": PIXI.JsonLoader,
+        "atlas": PIXI.AtlasLoader,
+        "anim": PIXI.SpineLoader,
+        "xml":  PIXI.BitmapFontLoader,
+        "fnt":  PIXI.BitmapFontLoader
+    };
+
+
+};
+
+/**
+Fired when an item has loaded
+@event onProgress
+**/
+
+/**
+Fired when all the assets have loaded
+@event onComplete
+**/
+
+// constructor
+PIXI.AssetLoader.constructor = PIXI.AssetLoader;
+
+/**
+ * This will begin loading the assets sequentially
+ */
+PIXI.AssetLoader.prototype.load = function()
+{
+    var scope = this;
+
+  this.loadCount = this.assetURLs.length;
+
+    for (var i=0; i < this.assetURLs.length; i++)
+  {
+    var fileName = this.assetURLs[i];
+    var fileType = fileName.split(".").pop().toLowerCase();
+
+        var loaderClass = this.loadersByType[fileType];
+        if(!loaderClass)
+            throw new Error(fileType + " is an unsupported file type");
+
+        var loader = new loaderClass(fileName, this.crossorigin);
+
+        loader.addEventListener("loaded", function()
+        {
+            scope.onAssetLoaded();
+        });
+        loader.load();
+  }
+};
+
+/**
+ * Invoked after each file is loaded
+ * @private
+ */
+PIXI.AssetLoader.prototype.onAssetLoaded = function()
+{
+    this.loadCount--;
+  this.dispatchEvent({type: "onProgress", content: this});
+  if(this.onProgress) this.onProgress();
+
+  if(this.loadCount == 0)
+  {
+    this.dispatchEvent({type: "onComplete", content: this});
+    if(this.onComplete) this.onComplete();
+  }
+};
+
---
+++ public/js/vendor/pixi/Utils/Utils.js
@@ -0,0 +1,95 @@
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+
+// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+
+// MIT license
+
+
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+
+window.requestAnimFrame = window.requestAnimationFrame;
+
+function HEXtoRGB(hex) {
+	return [(hex >> 16 & 0xFF) / 255, ( hex >> 8 & 0xFF) / 255, (hex & 0xFF)/ 255];
+}
+
+/**
+ * Provides bind in a cross browser way.
+ */
+if (typeof Function.prototype.bind != 'function') {
+  Function.prototype.bind = (function () {
+    var slice = Array.prototype.slice;
+    return function (thisArg) {
+      var target = this, boundArgs = slice.call(arguments, 1);
+ 
+      if (typeof target != 'function') throw new TypeError();
+ 
+      function bound() {
+	var args = boundArgs.concat(slice.call(arguments));
+	target.apply(this instanceof bound ? this : thisArg, args);
+      }
+ 
+      bound.prototype = (function F(proto) {
+          proto && (F.prototype = proto);
+          if (!(this instanceof F)) return new F;          
+	})(target.prototype);
+ 
+      return bound;
+    };
+  })();
+}
+
+var AjaxRequest = PIXI.AjaxRequest = function()
+{
+	var activexmodes = ["Msxml2.XMLHTTP", "Microsoft.XMLHTTP"] //activeX versions to check for in IE
+	
+	if (window.ActiveXObject)
+	{ //Test for support for ActiveXObject in IE first (as XMLHttpRequest in IE7 is broken)
+		for (var i=0; i<activexmodes.length; i++)
+		{
+			try{
+				return new ActiveXObject(activexmodes[i])
+			}
+   			catch(e){
+    			//suppress error
+   			}
+		}
+	}
+	else if (window.XMLHttpRequest) // if Mozilla, Safari etc
+  	{
+  		return new XMLHttpRequest()
+ 	}
+ 	else
+ 	{
+		return false;
+ 	}
+}
+
+
+
+
+
+
+
