75ba7eca49a1acb48af2ce8f2bf82d15afb01b8b
Maciej Chmurski <contact@t2n.eu>
2013-11-10 11:30:54 +0100

    level generation - full bars every 100 + on start

--- modules/level.js
+++ modules/level.js
@@ -1,23 +1,12 @@
-/* globals exports, require */
-var _ = require('lodash');
+/* globals exports */
 
-var defaults = {
-    ledgesCount: Math.floor(Math.random() * (600 - 400 + 1)) + 400
-};
-
-exports.generate = function (config) {
-    var lCount;
-    config = config || {};
-    _.defaults(config, defaults);
-
-    lCount = config.ledgesCount;
-
-    var level = [];
-    var line = [];
+function getLine(fullLine) {
     var last = 0;
-
-    for (var i = 0; i < lCount; i += 1) {
-        for (var n = 0; n < 10; n += 1) {
+    var line = [];
+    for (var n = 0; n < 11; n += 1) {
+        if (fullLine) {
+            line.push(1);
+        } else {
             if (last === 1) {
                 last = Math.round(Math.random() + 0.6);
                 line.push(last);
@@ -25,12 +14,36 @@ exports.generate = function (config) {
             } else {
                 last = Math.round(Math.random() - 0.2);
                 line.push(last);
-
             }
         }
-        level.push(line);
-        line = [];
     }
+    return line;
+}
+
+exports.generate = function() {
+    // config = config || {};
+    // _.defaults(config, defaults);
+
+    var level = [];
+    var i = 0;
+    // first level
+    level.push(getLine(true));
+    for (i = 0; i < 99; i += 1) {
+        level.push(getLine());
+    }
+
+    // seconds level
+    level.push(getLine(true));
+    for (i = 0; i < 99; i += 1) {
+        level.push(getLine());
+    }
+    // third level
+    level.push(getLine(true));
+    for (i = 0; i < 99; i += 1) {
+        level.push(getLine());
+    }
+    // finish
+    level.push(getLine(true));
 
     return level;
 };
